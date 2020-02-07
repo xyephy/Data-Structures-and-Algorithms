e19c16fd9d204def6c6c2c3aa88e19ccddb9e24b
xyephy <ziphyr.xyeag@gmail.com>
2020-02-08 00:12:16 +0300

    Hello World

---
+++ helloWorld.go
@@ -0,0 +1,13 @@
+//main package has examples shown
+// in Go Data Structures and algorithms book
+package main
+
+// importing fmt package
+import (
+	"fmt"
+)
+
+// main method
+func main() {
+	fmt.Println("Hello, World")
+}
