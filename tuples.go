a440aefc0ef8d378af81ae438a2ef19b0da7e6d2
xyephy <ziphyr.xyeag@gmail.com>
2020-02-08 00:21:07 +0300

    tuples

---
+++ tuples.go
@@ -0,0 +1,13 @@
+//main package has examples shown
+// in Hands-On Data Structures and algorithms with Go book
+package main
+
+// importing fmt package
+
+//gets the power series of integer a and returns tuple of square of a
+// and cube of a
+func powerSeries(a int) (int, int) {
+
+	return a * a, a * a * a
+
+}
